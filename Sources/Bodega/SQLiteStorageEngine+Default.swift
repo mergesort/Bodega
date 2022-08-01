e6530bf7f9919458877503a41ff1334232de684c
Joe Fabisevich <joe@fabisevi.ch>
2022-08-01 15:16:39 -0400

    Adding default(appendingPath) method for SQLiteStorageEngine

--- Sources/Bodega/SQLiteStorageEngine+Default.swift
+++ Sources/Bodega/SQLiteStorageEngine+Default.swift
@@ -4,4 +4,8 @@ public extension SQLiteStorageEngine {
         SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "Data"))!
     }
 
+    static func `default`(appendingPath pathComponent: String) -> SQLiteStorageEngine {
+        SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: pathComponent))!
+    }
+
 }
