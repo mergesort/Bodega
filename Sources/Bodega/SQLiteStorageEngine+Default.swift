d351e836a3f77aa26d0912ca29441e4ca2dad588
Joe Fabisevich <joe@fabisevi.ch>
2022-08-01 15:21:23 -0400

    Adding documentation for default storage engine property/function

--- Sources/Bodega/SQLiteStorageEngine+Default.swift
+++ Sources/Bodega/SQLiteStorageEngine+Default.swift
@@ -1,9 +1,18 @@
 public extension SQLiteStorageEngine {
 
+    /// An `SQLiteStorageEngine` located in the platform-specific default storage directory.
+    ///
+    /// Equivalent to:
+    /// `SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "Data"))`
     static var `default`: SQLiteStorageEngine {
         SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "Data"))!
     }
 
+    /// An `SQLiteStorageEngine` located in the platform-specific default storage directory.
+    ///
+    /// Equivalent to:
+    /// `SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: "Your Path"))`
+    /// - Parameter pathComponent: The path to append to the platform-specific defatult storage directory.
     static func `default`(appendingPath pathComponent: String) -> SQLiteStorageEngine {
         SQLiteStorageEngine(directory: .defaultStorageDirectory(appendingPath: pathComponent))!
     }
